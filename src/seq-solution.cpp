09c912208cd2ce83dd11d07c0d251688d7e8072b
William <williamq@andrew.cmu.edu>
2019-12-02 02:45:00 -0500

    sequential version works for now

--- src/seq-solution.cpp
+++ src/seq-solution.cpp
@@ -2,64 +2,87 @@
 #include <string>
 #include <vector>
 #include "pixel.h"
-#include "metadata.h"
 #include "imgSeg.h"
+#include <cstdint>
+#include <assert.h>
 
+int global_width;
+int global_height;
 inline bool mergeCriterion(Pixel p1, Pixel p2, int t){
-  return (p1.r - p2.r)*(p1.r - p2.r) + (p1.g - p2.g)*(p1.g - p2.g) +
-    (p1.b - p2.b) * (p1.b - p2.b) < t*t;
+  return ((int)p1.r - (int)p2.r)*((int)p1.r - (int)p2.r) + ((int)p1.g - (int)p2.g)*((int)p1.g -(int) p2.g) +
+    ((int)p1.b - (int)p2.b) * ((int)p1.b - (int)p2.b) < t*t;
 }
 
-Pixel newColor(Pixel A, Pixel B, Metadata findA, Metadata findB){
-  int totalSize = findA.size + findB.size;
+Pixel newColor(Pixel A, Pixel B, int sizeA, int sizeB){
+  int totalSize = sizeA + sizeB;
   Pixel newP;
-  newP.r = (A.r * findA.size + B.r * findB.size)/(totalSize);
-  newP.g = (A.g * findA.size + B.g * findB.size)/(totalSize);
-  newP.b = (A.b * findA.size + B.b * findB.size)/(totalSize);
-
+  newP.r = (uint8_t)(((int)A.r * sizeA +(int) B.r * sizeB)/(totalSize));
+  newP.g = (uint8_t)(((int)A.g * sizeA + (int)B.g * sizeB)/(totalSize));
+  newP.b = (uint8_t)(((int)A.b * sizeA + (int)B.b * sizeB)/(totalSize));
+  //printf("A r: %d A g: %d A b: %d B r: %d B g: %d B b: %d A size: %d B size: %d n r: %d n g: %d n b: %d\n",
+  //    A.r,A.g,A.b,B.r,B.g,B.b,findA.size,findB.size,newP.r,newP.g,newP.b);
   return newP;
 }
 
-Metadata find(std::vector<std::vector<Metadata>> &meta,int srow,int scol){
+int find(std::vector<std::vector<int>> &next,int srow,int scol){
   int row = srow;
   int col = scol;
   while(1){
-    Metadata m = meta[row][col];
-    if(m.nrow == row && m.ncol == col){
-      meta[srow][scol].nrow = row;
-      meta[srow][scol].ncol = col;
-      return m;
+    //std::cout<<"in find"<<std::endl;
+    int index = next[row][col];
+    if(index == -1){
+      int actualIndex = row *global_width + col;
+      next[row][col] = actualIndex;
+      next[srow][scol] = actualIndex;
+      return actualIndex;
     }
-    else if (meta[row][col].nrow == -1 && meta[row][col].ncol == -1){
-      meta[row][col].nrow = row;
-      meta[row][col].ncol = col;
-      meta[srow][scol].nrow = row;
-      meta[srow][scol].ncol = col;
-      return meta[row][col];
+    int nextRow = index/global_width;
+    int nextCol = index % global_width;
+    if(nextRow == row && nextCol == col){
+      next[srow][scol] = index;
+      return index;
     }
-    row = meta[row][col].nrow;
-    col = meta[row][col].ncol;
+    row = nextRow;
+    col = nextCol;
+    //std::cout<<"here"<<std::endl;
   }
 }
 
-void verify_edge(std::vector<std::vector<Pixel>> &pixels, std::vector<std::vector<Metadata>> &meta, int row1, int col1, int row2, int col2) {
-  Metadata findA = find(meta,row1,col1);
-  Metadata findB = find(meta,row2,col2);
-  if(findA.nrow != findB.nrow || findA.ncol != findB.ncol){
-    Pixel A = pixels[findA.nrow][findA.ncol];
-    Pixel B = pixels[findB.nrow][findB.ncol];
-    if(mergeCriterion(A,B,4)){
-      if(findA.size>findB.size){
-        pixels[findA.nrow][findA.ncol] = newColor(A,B, findA, findB);
-        findB.nrow = findA.nrow;
-        findB.ncol = findA.ncol;
-        findA.size += findB.size;
+void verify_edge(std::vector<std::vector<Pixel>> &pixels, std::vector<std::vector<int>> &next,
+    std::vector<std::vector<int>> &size, int col1, int row1, int col2, int row2) {
+  //sanity check
+  assert(col1< global_width && col1>=0 && col2 < global_width && col2 >= 0 && row1 < global_height && row1 >=0
+      && row2 < global_height && row2 >=0);
+
+  int aIndex = find(next,row1,col1);
+  int aRow = aIndex / global_width;
+  int aCol = aIndex % global_width;
+
+  int bIndex = find(next,row2,col2);
+  int bRow = bIndex / global_width;
+  int bCol = bIndex % global_width;
+
+  if(aRow != bRow || aCol != bCol){
+    Pixel A = pixels[aRow][aCol];
+    int aSize = size[aRow][aCol];
+
+    Pixel B = pixels[bRow][bCol];
+    int bSize = size[bRow][bCol];
+
+    if(mergeCriterion(A,B,30)){
+      printf("A r: %d g: %d b: %d B r: %d g: %d b: %d\n",A.r,A.g,A.b,B.r,B.g,B.b);
+      if(aSize>bSize){
+        pixels[aRow][aCol] = newColor(A,B, aSize, bSize);
+        printf("newCol r: %d g: %d b: %d\n",pixels[aRow][aCol].r,pixels[aRow][aCol].g,pixels[aRow][aCol].b);
+        next[bRow][bCol] = aIndex;
+        size[aRow][aCol] += bSize;
       }
       else{
-        pixels[findB.nrow][findB.nrow] = newColor(A, B, findA, findB);
-        findA.nrow = findB.nrow;
-        findA.ncol = findB.ncol;
-        findB.size += findB.size;
+        pixels[bRow][bCol] = newColor(A, B, aSize, bSize);
+
+        printf("newCol r: %d g: %d b: %d\n",pixels[aRow][aCol].r,pixels[aRow][aCol].g,pixels[aRow][aCol].b);
+        next[aRow][aCol] = bIndex;
+        size[bRow][bCol] += aSize;
       }
     }
   }
@@ -67,58 +90,71 @@ void verify_edge(std::vector<std::vector<Pixel>> &pixels, std::vector<std::vecto
 
 }
 
-
 void process(std::vector<std::vector<Pixel>>& pixels,int width, int height){
-  std::vector<std::vector<Metadata>> meta(height,std::vector<Metadata>(width));
+  global_height = height;
+  global_width = width;
+  std::vector<std::vector<int>> next(height,std::vector<int>(width,-1));
+  std::vector<std::vector<int>> size(height,std::vector<int>(width,1));
   int start = 0;
   int offset = 2;
   int limit;
   while(start < width - 1 || start < height -1){
     //Comparing along row
     for(int y =0;y<height;y++){
-      for(int x = start;x<width;x+=offset){
-        verify_edge(pixels,meta,x,y,x+1,y);
+      for(int x = start;x<=width-offset;x+=offset){
+        verify_edge(pixels,next,size,x,y,x+1,y);
       }
     }
+    std::cout<<"row comparison done"<<std::endl;
     int count = 0;
     for(int y = 0; y<height; y*=2){
       if(count == 1){
         y = offset/2;
       }
-      for(int x = start;x<width;x+=offset){
+      for(int x = start;x<=width-offset;x+=offset){
         limit = offset/2 - 1;
         //guarantee y+limit <= height
         if(y + limit > height){
           limit = height - y -1;
         }
         for(int n=0;n<limit;n++){
-          verify_edge(pixels,meta,x,y+n,x+1,y+n+1);
-          verify_edge(pixels,meta,x+1,y+n,x,y+n+1);
+          verify_edge(pixels,next,size,x,y+n,x+1,y+n+1);
+          verify_edge(pixels,next,size,x+1,y+n,x,y+n+1);
         }
       }
       count++;
     }
 
-    for ( int y = start; y<height; y += offset){
+    std::cout<<"second loop done"<<std::endl;
+    for ( int y = start; y<=height-offset; y += offset){
       for(int x =0 ; x< width; x++){
-        verify_edge(pixels,meta,x,y,x,y+1);
+        verify_edge(pixels,next,size,x,y,x,y+1);
       }  
     }
-
-    for(int y = start; y < height; y+= offset){
-      for(int x = 0; x<width; x+=offset){
+    std::cout<<"third loop done"<<std::endl;
+    for(int y = start; y <= height-offset; y+= offset){
+      for(int x = 0; x<= width-offset; x+=offset){
         limit = offset -1;
         if(x+limit>width){
           limit =width - x -1;
         }
         for( int n = 0;n<limit;n++){
-          verify_edge(pixels,meta,x+n,y,x+n+1,y+1);
-          verify_edge(pixels,meta,x+n+1,y,x+n,y+1);
+          verify_edge(pixels,next,size,x+n,y,x+n+1,y+1);
+          verify_edge(pixels,next,size,x+n+1,y,x+n,y+1);
         }
       }
     }
     start = 2*(start+1)-1;
     offset *=2;
+  }
+
+  for(int i=0;i<height;i++){
+    for(int j=0;j<width;j++){
+      int index = find(next,i,j);
+      int row = index / global_width;
+      int col = index % global_width;
+      pixels[i][j]=pixels[row][col];
+    }
   }
   return;
 }
